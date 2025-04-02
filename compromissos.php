b122c643c687e1c868c52e9addd37bb91b50c676
luis felipe <luisfelipecasagrande878@gmai.com>
2025-04-01 22:33:07 -0300

    ordenação dos compromissos

--- compromissos.php
+++ compromissos.php
@@ -3,13 +3,51 @@
 require_once('twig_carregar.php');
 
 require('inc/banco.php');
+$data = $_GET['data'] ?? null;
 
-//busca as compras no banco
-$dados = $pdo->query('SELECT * FROM compromissos');
+if ($data == 'recente'){
 
-$compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
+    $dados = $pdo->query('SELECT * FROM compromissos ORDER BY data ASC');
 
-echo $twig->render('compromissos.html' , [
-    'titulo' => 'compromissos',
-    'compromissos' => $compromissos,
-]);
+    $compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
+
+    echo $twig->render('compromissos.html' , [
+        'titulo' => 'compromissos',
+        'compromissos' => $compromissos,
+    ]);
+
+} else if ($data == 'distante') {
+
+    $dados = $pdo->query('SELECT * FROM compromissos ORDER BY data DESC');
+
+    $compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
+
+    echo $twig->render('compromissos.html' , [
+        'titulo' => 'compromissos',
+        'compromissos' => $compromissos,
+    ]);
+
+} else if ($data == 'alfabeto'){
+
+    $dados = $pdo->query('SELECT * FROM compromissos ORDER BY titulo ASC');
+
+    $compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
+
+    echo $twig->render('compromissos.html' , [
+        'titulo' => 'compromissos',
+        'compromissos' => $compromissos,
+    ]);
+
+} else {
+
+    //busca as compras no banco
+    $dados = $pdo->query('SELECT * FROM compromissos');
+
+    $compromissos = $dados->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo $twig->render('compromissos.html' , [
+        'titulo' => 'compromissos',
+        'compromissos' => $compromissos,
+    ]);
+
+}
